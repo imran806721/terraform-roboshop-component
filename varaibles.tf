b280310426c67bdb1231b3b2cd8566dac64dc678
imran <imran806721@gmail.com>
2026-07-11 13:53:58 +0530

    terraform

---
+++ varaibles.tf
@@ -0,0 +1,23 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+    default = "v3"
+}
+
+variable "component" {
+    type = string
+}
+
+variable "domain_name" {
+    default = "devops90s.shop"
+}
+
+variable "rule_priority" {
+    #type = string
+}
