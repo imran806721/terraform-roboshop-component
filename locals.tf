971a3b419662fdb438fe6a52cfba1af29691bcf8
imran <imran806721@gmail.com>
2026-07-13 10:42:12 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -6,6 +6,8 @@ locals {
     common_name = "${var.project}-${var.environment}-${var.component}"
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
+     host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = {
         Project = "${var.project}"
         Environment = "${var.environment}"
